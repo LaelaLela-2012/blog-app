d8532b675ed6643d49ddbe60ab1b17babd2625c7
unknown <2012laela@gmail.com>
2024-02-29 22:26:46 +0800

    App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Write from './pages/Write';
+import Home from './pages/Home';
+import Single from './pages/Single';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>This is Home!</div>,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/write",
+    element: <Write/>,
+  },
+  {
+    path: "/single",
+    element: <Single/>,
+  },
+]);
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router} />
+      {/* <h1>Hello Almeera</h1> */}
     </div>
   );
 }
